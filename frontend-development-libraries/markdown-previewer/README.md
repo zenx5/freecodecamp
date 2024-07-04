de77367e1d6bfa3fe0f1b13f494868d1d80739b2
Octavio Martinez <omartinez1618@gmail.com>
2024-07-04 13:43:48 -0400

    fc for markdown previewer

---
+++ frontend-development-libraries/markdown-previewer/README.md
@@ -0,0 +1,13 @@
+Historia de usuario n.° 1: Puedo ver un textareaelemento con un id="editor".
+
+Historia de usuario n.° 2: Puedo ver un elemento con un id="preview".
+
+Historia de usuario n.° 3: cuando ingreso texto en el #editorelemento, #previeweste se actualiza a medida que escribo para mostrar el contenido del área de texto.
+
+Historia de usuario n.° 4: cuando ingreso Markdown con sabor a GitHub en el #editorelemento, el texto se representa como HTML en el #previewelemento a medida que escribo (SUGERENCIA: no necesita analizar Markdown usted mismo; puede importar la biblioteca Marked para esto: https://cdnjs.com/libraries/marked ).
+
+Historia de usuario n.° 5: Cuando mi vista previa de Markdown se carga por primera vez, el texto predeterminado en el #editorcampo debe contener Markdown válido que represente al menos uno de cada uno de los siguientes elementos: un elemento de encabezado (tamaño H1), un elemento de subtítulo (tamaño H2), un enlace, código en línea, un bloque de código, un elemento de lista, una cita en bloque, una imagen y texto en negrita.
+
+Historia de usuario n.° 6: Cuando mi vista previa de Markdown se carga por primera vez, el Markdown predeterminado en el #editorcampo debe representarse como HTML en el #previewelemento.
+
+Bono opcional (no es necesario que esta prueba pase): Mi previsualizador de Markdown interpreta los retornos de carro y los representa como brelementos (salto de línea).
---
+++ frontend-development-libraries/markdown-previewer/src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import { parse } from 'marked'
+import textDefault from './default.txt'
+
+
+export default function App() {
+  const [content, setContent] = useState("")
+  const [parsed, setParsed] = useState("")
+
+  useEffect(()=>{
+    fetch(textDefault)
+    .then( response => response.text() )
+    .then( text => setContent(text) )
+  },[])
+
+  useEffect(()=>{
+    (async ()=>{
+      setParsed( await parse(content, { gfm:true, breaks:true }) )
+    })()
+  },[content])
+
+
+  const handlerChangeContent = async (event:{ target: { value:string }}) => {
+    setContent( event.target.value )
+    setParsed( await parse(event.target.value, { gfm:true, breaks:true }) )
+  }
+
+  return <div className="w-screen h-min-screen flex items-center flex-col justify-center gap-8 pt-20">
+    <div className="bg-slate-300 max-w-4xl w-full h-60 flex flex-col border border-black shadow-xl shadow-slate-800">
+      <label className="border-b border-black border-opacity-50 py-2 px-4 font-bold text-lg">Editor</label>
+      <textarea id="editor" className="bg-slate-300 w-full h-60 p-4" value={content} onChangeCapture={handlerChangeContent}></textarea>
+    </div>
+    <div className="bg-slate-300 max-w-4xl w-full flex flex-col   shadow-xl shadow-slate-800 border border-black">
+      <label className="border-b border-black border-opacity-50 py-2 px-4 font-bold text-lg">Preview</label>
+      <div id="preview" className="bg-slate-300 p-4" dangerouslySetInnerHTML={{ __html:parsed }} />
+      {/* <div id="preview" className="bg-slate-300 p-4">{parsed}</div> */}
+    </div>
+  </div>
+}
+
+
